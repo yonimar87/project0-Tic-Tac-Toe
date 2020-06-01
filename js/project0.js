b29a20d9909a256181008a1e534043125b1288e3
yonathan marlow <yonathanmarlow@hotmail.com>
2020-06-01 20:52:55 +1000

    first committ

---
+++ js/project0.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+const player1 = 
+
+$('#button').on('click', function() {
+
+
+})
+
+
+set the default avatars for both
+start button work
+whateer is clicked is what comes up.
+        every turn = change between players
+
+check win conditions
+3 functions - check horizontal / vertical / diagonal.
+which will be array in array.
+for loops will come around here as well.
+
+once there is a win condition
+end the game with new prompt = "Congrats .... " -- if player one, if player 2 wins
+
+if not win condition = draw.
+---function that makes sure all cells are tagged.
+can be done with default value of -1.
+
+let cells = [
+  ['1', '2' '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9']
+];
+
+})
+
+for (var i = 0; i < cells.length; i++) {
+    for (var j = 0; j <cells[i].length; j++) {
+    }
+}
+
+cells[i][j]
