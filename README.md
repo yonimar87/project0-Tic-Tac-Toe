ed1597497ab5270b1196f2fae41209a1cc1b1e97
yonathan marlow <yonathanmarlow@hotmail.com>
2020-06-04 12:21:37 +1000

    new commit - mario theme added

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tic Tac Toe - Yoni's Version
+---
+
+This was quite an interesting project. It was pretty great to see how many different ways people can code this game and obviously the different CSS and JavaScript functionalities.
+
+To access the game please visit the following link:
+https://yonimar87.github.io/project0-Tic-Tac-Toe/A
+
+To complete the game the following languages were used:
+
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+The way I approached the project was to initially build an HTML and CSS attributes to ensure I have a working table to be able to implement the actual game onto. Once this was esablished I started with JavaScript and jquery
+
+The plan was to come up with variables and then the functions that will be using them. I have a couple of global functions and variables, while the rest of the functions are all within the working one.
+
+I've used hidden and visibility + opacity to make animations happen on the side of the screen once we click on the start button.
+
+in order to select an image and use it for the table, I used outerHTML from JavaScript to convert the image to a string and then input the string into the cell. This is used by using the 'this' attribute so we know which image is selected and also which box is selected to do so.
+
+In order for a box to not be used twice, I've got an empty array that is filled with the id's of the images - once there is an id in the box, another one cannot be added.
+
+Turns are determined via a counter. Each even turn is player 1, while odd turns are player two.
+
+I've set up a function that once 5 wins have been established, the entire game restarts (after having an alert advising of who won based on which turn acted last )
--- js/project0.js
+++ js/project0.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
   // al the variables i have
-  // let tokensLeft = {
-  //   mario: `<img id="mario" src="mario.jpeg" alt="Mario">`,
-  //   Luigi: `<img id="luigi" src="luigi.jpeg" alt="Luigi">`,
-  //   Bowser: `<img id="bowser" src="bowser.jpg" alt="Bowser">`
-  // }
-  // let tokensRight = {
-  //   Kodama: `<img id="kodama" src="kodama.jpeg" alt="Kodama">`,
-  //   noface:`<img id="noface" src="noface.jpeg" alt="No Face">`,
-  //   ironGiant: `<img id="irongiant" src="irongiant.jpeg" alt="Iron Giant">`
-  // }
-  let playerOne = `X`
-  let playerTwo = `0`
+  let playerOne = `<img class='avatar1' id="xDefault" src="X.jpg" alt="X">`
+  let playerTwo = `<img class='avatar2' id="circleDefault" src="O.png" alt="0">`
   let counter = 0;
   let playerOneWins = 0;
   let playerTwoWins = 0;
@@ -32,10 +22,16 @@ $(document).ready(function() {
     ['02', '04', '06']
   ]
 
-
+  const refreshGame = function (){
+    $('.box').empty();
+    usedCells = [];
+    counter = 0;
+    playerOneChoices = [];
+    playerTwoChoices = [];
+  }
 
   const getInputValue = function() {
-    inputVal2 = document.getElementById("player2Name").value;
+    inputVal2 = $("#player2Name").val();
     inputVal = document.getElementById("player1Name").value;
     $('#leftWins').html(inputVal);
     $('#rightWins').html(inputVal2);
@@ -59,23 +55,24 @@ $(document).ready(function() {
     }).animate({
       opacity: 0.9
     }, 2000);
-    $('.box').empty();
-    usedCells = [];
-    counter = 0;
-    playerOneChoices = [];
-    playerTwoChoices = [];
+    refreshGame()
+
+    $('#mariotheme')[0].play();
+
     $('#game, .duringGame').css({
       opacity: 0,
       visibility: 'visible'
     }).animate({
       opacity: 1.0
     }, 2000);
+
     $('#gamesDrawn').css({
       opacity: 0,
       visibility: 'visible'
     }).animate({
       opacity: 1.0
     }, 2000);
+
     $('.startButton').text('Click Here to Restart the round');
   }
 
@@ -86,14 +83,16 @@ $(document).ready(function() {
   //means I can have less popups.
 
   // PRETTY MUCH ENTIRE GAME.
+
+  //this is to make sure that the image i choose is then added to my sides for when the game is played
   const imagePicker1 = function() {
     playerOne = this.outerHTML
-    $('this').addClass('.selectedAvatar');
+    // $(playerOne).toggleClass('.selectedAvatar');
   }
 
   const imagePicker2 = function() {
     playerTwo = this.outerHTML
-    $('this').addClass('.selectedAvatar');
+    // $(playerTwo).toggleClass('.selectedAvatar');
   }
 
   $('.avatar1').on('click', imagePicker1);
@@ -145,15 +144,14 @@ $(document).ready(function() {
       }
       if (usedCells.length === 9) {
         draws++;
-        $('.box').empty();
-        usedCells = [];
-        counter = 0;
-        playerOneChoices = [];
-        playerTwoChoices = [];
+        refreshGame()
         return 'Its a draw!';
       }
       return 'no results yet';
     };
+
+
+
 
     // CALLING WINNING COMBO - PROVIDING THE WINNING LINES/ALERTS
     const result = winningCombo()
@@ -170,6 +168,7 @@ $(document).ready(function() {
 
   })
 
+//function for tallying the scores.
   const tallyScores = function() {
     if (playerOneWins === 5) {
       playerOneWins = 0;
@@ -183,13 +182,8 @@ $(document).ready(function() {
       alert(`Congratulations ${inputVal2}, you are the first to 5 wins!!!`)
 
     }
-    $('.box').empty();
-    usedCells = [];
-    counter = 0;
-    playerOneChoices = [];
-    playerTwoChoices = [];
+    refreshGame();
   }
-
 
 })
 
